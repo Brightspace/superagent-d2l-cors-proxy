53ce091bc84faba27d0b12ef5fef67d528c47b24
Dave Lockhart <dave.lockhart@d2l.com>
2015-05-13 12:41:37 -0400

    switching to an IFRAME-based model with postMessage

--- index.js
+++ index.js
@@ -1,26 +1,146 @@
 'use strict';
 
-var headerName = 'X-CorsProxy-Url';
+var requests = {};
+var queuedRequests = [];
+var ready = false;
+var id = 0;
+var iframe = null;
+var version = require('./package.json').version;
 
-module.exports = function(req) {
+function send(msg) {
+
+	requests[msg.id] = msg;
+
+	if(!ready) {
+		queuedRequests.push(msg.id);
+		return;
+	}
+
+	iframe.contentWindow.postMessage(
+		JSON.stringify(msg),
+		'*'
+	);
+
+}
+
+function sendQueuedRequests() {
+	queuedRequests.forEach(function(id) {
+		send(requests[id]);
+	});
+	queuedRequests = [];
+}
+
+function onMessage(evt) {
+
+	if(evt.source !== iframe.contentWindow) {
+		return;
+	}
+
+	var data = JSON.parse(evt.data);
+
+	if(data.type === 'ready') {
+		ready = true;
+		sendQueuedRequests();
+		return;
+	}
+
+	var r = requests[data.id];
+	if(r) {
+		r._this.callback.call(r._this, data.err, data.res);
+		delete requests[data.id];
+	}
+
+}
+
+function tryGetHost(url) {
+
+	url = url || '';
+
+	var host = url;
+	if(url.indexOf('//') === 0) {
+		host = url.substr(2);
+	} else if(url.indexOf('http://') === 0) {
+		host = url.substr(7);
+	} else if(url.indexOf('https://') ===0) {
+		host = url.substr(8);
+	} else {
+		return null;
+	}
+
+	var scheme = url.substr(0,(url.length-host.length));
+
+	var index = host.indexOf('/');
+	if(index > -1) {
+		host = host.substr(0, index);
+	}
+
+	return scheme + host;
+
+}
+
+function buildFrame(host) {
+	iframe = document.getElementById('d2l-cors-proxy');
+	if(iframe === null ) {
+		iframe = document.createElement('iframe');
+		iframe.id = 'd2l-cors-proxy';
+		iframe.setAttribute('style', 'display:none;');
+		iframe.src = host + '/lib/d2l-cors-proxy/' + version + '/';
+		document.body.appendChild(iframe);
+	}
+}
+
+module.exports = function(superagent) {
 
 	// CORS feature detection:
 	// http://stackoverflow.com/questions/1641507/detect-browser-support-for-cross-domain-xmlhttprequests
 	// https://hacks.mozilla.org/2009/07/cross-site-xmlhttprequest-with-cors/
-	var val = 'withCredentials' in new XMLHttpRequest();
-	if(val) {
-		return req;
+	var supportsCors = 'withCredentials' in new XMLHttpRequest();
+	if(supportsCors) {
+		return function(req) {
+			return req;
+		};
 	}
 
-	// only proxy requests to CDN over HTTP or HTTPS
-	var url = req.url.trim().toLowerCase();
-	if(url.indexOf('https://s.brightspace.com') !== 0 && url.indexOf('http://s.brightspace.com') !== 0 ) {
+	var oldEnd = superagent.Request.prototype.end;
+
+	superagent.Request.prototype.end = function(fn) {
+
+		var host = tryGetHost(this.url);
+		if(!this._d2lCorsProxy || !host) {
+			oldEnd.apply(this, arguments);
+			return this;
+		}
+
+		buildFrame(host);
+
+		this._callback = fn || function(){};
+
+		var msg = {
+			id: ++id,
+			method: this.method,
+			url: this.url,
+			query: this._query,
+			timeout: this._timeout,
+			data: this._data,
+			header: this.header,
+			_this: this
+		};
+		send(msg);
+
+	};
+
+	window.addEventListener('message', onMessage);
+
+	return function(req) {
+		req._d2lCorsProxy = true;
 		return req;
-	}
-
-	req.set(headerName, req.url);
-	req.url = '/d2l/lp/cors-proxy';
-
-	return req;
+	};
 
 };
+module.exports.getProxyFilePath = function() {
+	return __dirname + '/index.html';
+};
+module.exports.getProxyDefaultLocation = function() {
+	return '/lib/d2l-cors-proxy/' + version + '/';
+};
+module.exports._tryGetHost = tryGetHost;
